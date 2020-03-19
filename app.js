2762568cec9aab0a979841848ae079a707daad61
Arindam Lahiri <arindamlahiri17@gmail.com>
2020-03-19 21:24:22 +0530

    registration

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const bcrypt = require('bcryptjs')
 const ejs = require('ejs')
 const dotenv = require('dotenv')
 dotenv.config()
+const {registerValidation,loginValidation} = require('./validation')
 
 
 app.use(express.urlencoded({extended:false}))
@@ -23,11 +24,47 @@ const mongoClient = mongodb.MongoClient
 const url = process.env.DB_URL
 
 app.get("/", (req,res) => {
-    res.render("login")
+    res.render("login",{msg:''})
 })
 
 app.get('/register', (req,res) => {
-    res.render('register')
+    res.render('register',{msg:''})
+})
+
+app.post('/register',(req,res) => {
+    const {error} = registerValidation(req.body)
+
+    if(error) return res.status(400).render('register',{msg:error.details[0].message})
+
+    mongoClient.connect(url, {useUnifiedTopology:true}, (err,db) => {
+        if(err) throw err
+
+        let dbo = db.db('atom')
+
+        dbo.collection('non-member').find({email:req.body.email}).toArray((dbErr,result) => {
+            if(dbErr) throw dbErr
+
+            if(result.length) return res.status(400).render('register',{msg:"Email already exists"})
+
+            const salt = bcrypt.genSaltSync(10)
+            const hashed = bcrypt.hashSync(req.body.password,salt)
+
+            const newData = {
+                name:req.body.name,
+                regno:req.body.regno,
+                year:req.body.year,
+                email:req.body.email,
+                password:hashed
+            }
+
+            dbo.collection('non-member').insertOne(newData, (dbErr,result) => {
+                if(dbErr) throw dbErr
+
+                console.log("Registered")
+            })
+            res.render('login',{msg:"You have been registered. Login to continue."})
+        })
+    })
 })
 
 app.post('/login', (req,res) => {
---
+++ validation.js
@@ -0,0 +1,25 @@
+const Joi = require('@hapi/joi')
+
+const registerValidation = data => {
+    const schema = Joi.object({
+        name:Joi.string().min(3).required(),
+        email:Joi.string().required().email(),
+        regno:Joi.string().required().min(15).max(15),
+        year:Joi.number().required(),
+        password:Joi.string().min(8).required()
+    })
+
+    return schema.validate(data)
+}
+
+const loginValidation = data => {
+    const schema = Joi.object({
+        email:Joi.string().required().email(),
+        password:Joi.string().min(8).required()
+    })
+
+    return schema.validate(data)
+}
+
+module.exports.registerValidation = registerValidation
+module.exports.loginValidation = loginValidation
