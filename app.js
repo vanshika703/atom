da446ec3075f2a6e76c184397ad00e27ae845e3f
Arindam Lahiri <arindamlahiri17@gmail.com>
2020-03-25 22:53:21 +0530

    change password and sessions

--- app.js
+++ app.js
@@ -62,13 +62,61 @@ app.post('/adminLogin', (req,res) => {
         let dbo = db.db('atom')
 
         dbo.collection('admin').find({email:req.body.email}).toArray((dbErr,result) => {
-            if(!result.length || !bcrypt.compareSync(req.body.password,result[0].password))
-                return res.status(400).send("Email/Password is wrong")
+            if(!result.length || !bcrypt.compareSync(req.body.password,result[0].password)) return res.status(400).send("Email/Password is wrong")
 
-            res.status(200).send("logged in")
+            req.session.user = {
+                name:result[0].name,
+                email:result[0].email,
+                id:result[0]._id
+            }
+
+            req.session.user.expires = new Date(
+                Date.now() + 1000 * 60 * 60 * 2
+            )
+            res.end()
         })
     })
 })
 
+app.use((req,res,next) => {
+    if(req.session.user) next()
+    else res.render('adminlogin',{msg:"Please login to continue"})
+})
+
+app.get('/admin/dashboard', (req,res) => {
+    res.render('admindashboard',{user:req.session.user})
+})
+
+app.get('/admin/changePassword', (req,res) => {
+    res.render('adminchangepassword')
+})
+
+app.post('/admin/changePassword',(req,res) => {
+    
+    mongoClient.connect(url, {useUnifiedTopology:true},(err,db) => {
+        let dbo = db.db('atom')
+
+        dbo.collection('admin').findOne({_id:new ObjectId(req.session.user.id)},(dbErr,user) => {
+            if(dbErr) throw dbErr
+
+            if(!bcrypt.compareSync(req.body.cpassword,user.password)) return res.status(400).send('Current Password is incorrect')
+
+            const salt = bcrypt.genSaltSync(10)
+            const hashed = bcrypt.hashSync(req.body.npassword,salt)
+            dbo.collection('admin').updateOne({_id:new ObjectId(req.session.user.id)},{$set:{password:hashed}}, (dbErr,result) => {
+                if(dbErr) throw dbErr 
+
+                console.log(result)
+                res.send('Password Updated')
+            })
+        })
+    })
+})
+
+app.get('/admin/logout', (req,res) => {
+    req.session.destroy()
+    res.render('adminlogin',{msg : "you have been logged out"})
+})
+
 var PORT = process.env.PORT || 5000
-app.listen(PORT, () => console.log("server up at 5000"))
+app.listen(PORT, () => console.log(`server up at ${PORT}`))
