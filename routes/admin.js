659af93b0a75ce34a57787f4f32fb1d6f62dc3de
Arindam Lahiri <arindamlahiri17@gmail.com>
2020-05-11 20:55:30 +0530

    removed async await from addTask

--- routes/admin.js
+++ routes/admin.js
@@ -214,10 +214,16 @@ Router.get('/addTask',(req,res) => {
 Router.post('/addTask',async(req,res) => {
     req.body.addedBy = req.session.user.name
 
-    const db = await mongoClient.connect(url,{useUnifiedTopology:true}).catch(err => console.log(err))
-    let result =  await db.db('atom').collection('tasks').insertOne(req.body).catch(err => console.log(err))
-    console.log(result.insertedCount)
-    res.send({msg:'Task added. Page reloading in 2 seconds'})
+    mongoClient.connect(url,{useUnifiedTopology:true},(err,db) => {
+        if(err) throw err
+        
+        db.db('atom').collection('tasks').insertOne(req.body,(dbErr,result) => {
+            if(dbErr) throw dbErr
+
+            console.log(result.insertedCount)
+            res.send({msg:'Task added. Page reloading in 2 seconds'})
+        })
+    })
 })
 
 
