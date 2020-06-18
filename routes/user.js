612a2262a361262c59f646809c3963b6819f9398
Arindam Lahiri <54589973+arindamlahiri@users.noreply.github.com>
2020-06-19 02:46:35 +0530

    integrated registeration and verification

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,138 @@
 const Router = require('express').Router()
+
 const mongodb = require('mongodb')
 const ObjectId = mongodb.ObjectId
+
+const bcrypt = require('bcryptjs')
+const nodemailer = require('nodemailer')
+const fetch = require('node-fetch')
+
+const {OAuth2Client} = require('google-auth-library')
+const client = new OAuth2Client("523384873779-e29ttamvfnbfkhb650ppufoas5qmr328.apps.googleusercontent.com")
+
 const { loginValidation,registerValidation } = require('../validation')
+
+let host
+let rand
+
+Router.get('/', (req,res)=>{
+    res.render('user/register')
+})
+
+Router.post('/register', async(req,res)=>{
+
+    const { email,password,captcha } = req.body
+
+    if(!captcha){
+        return res.status(400).json({"msg":"Please select captcha"})
+    }
+
+    const secretKey = '6Lfie_kUAAAAAAYZM0z0uetf7cs8pFQ4_e567GmN'
+
+    const verifyUrl = `https://google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${captcha}&remoteip=${req.connection.remoteAddress}`
+
+    let result = await fetch(verifyUrl)
+    let data = await result.json()
+
+    if(!data.success) return res.status(400).json({"msg":"Failed captcha verification! Please reload the page and try again"})
+
+    let { error } = registerValidation({ email,password })
+    if(error) return res.status(400).json({msg:error.details[0].message})
+
+    let db = req.app.locals.db
+    let dbo = db.db("atom")
+
+    dbo.collection('users').findOne({email}, async(err, user)=>{
+        if(err) return res.render('error')
+
+        //if user exists
+        if(user)
+            return res.status(400).json({msg:"This email is already registered!"})
+        else{
+            //hashing password
+            const salt = await bcrypt.genSalt(10)
+            const hashedPassword = await bcrypt.hash(password, salt)
+
+            //new user info
+            let userInfo = {
+                email,
+                password : hashedPassword,
+                userType : 0,
+                verified : 0,
+                registered : 0
+            }
+
+            dbo.collection('users').insertOne(userInfo, (dbErr,result)=>{
+                if(dbErr) return res.render('error')
+
+                host = req.get('host')
+                rand = result.insertedId
+                link = "http://"+req.get('host')+"/user/verify?id="+rand;
+
+                let transporter = nodemailer.createTransport({
+                    service: 'gmail',
+                    auth: {
+                        user: process.env.EMAIL,
+                        pass: process.env.PASSWORD
+                    },
+                    tls:{
+                        rejectUnauthorized:false
+                    }
+                });
+                
+                let mailOptions = {
+                    from: process.env.EMAIL,
+                    to: email,
+                    subject: 'Confirmation email for Tdian register',
+                    html: "<p>link is...<a href="+link+">Click here to verify....</a></p>"
+                };
+                
+                transporter.sendMail(mailOptions, function(error, info){
+                    if (error) {
+                        console.error(error);
+                    } else {
+                        console.log('Email sent: ' + info.response);
+                    }
+                });
+
+                res.json({msg:"Check email for verification link"})
+            })
+        }
+    })    
+})
+
+Router.get('/verify', (req,res) => {
+
+    console.log("link :"+req.protocol+":/"+req.get('host'))
+
+    if((req.protocol+"://"+req.get('host'))==("http://"+host)) {
+        console.log("Domain matched")
+
+        let db = req.app.locals.db
+        let dbo = db.db('atom')
+
+        dbo.collection('users').findOne({_id : new ObjectId(req.query.id)}, (dbErr, user)=>{
+            if(dbErr) return res.render('error')
+            
+                if(!user) return res.send('No such user found')
+
+                dbo.collection('users').updateOne({_id : new ObjectId(req.query.id)}, { $set: {verified : 1} }, async(err, result)=>{
+                    if(err) return res.render('error')
+
+                    console.log(result.modifiedCount)
+                    res.redirect('/user/login')
+                })
+            }) 
+    } 
+    else {
+        res.status(404).send('Error 404')
+    }
+})
+
+Router.get('/login', (req,res)=>{
+    res.render('user/login')
+})
+
 
 Router.get('/dash',async(req,res) => {
     let user = {
@@ -69,5 +200,19 @@ Router.put('/update/:id',async(req,res) => {
     }
 
 })
+
+function auth(req,res,next){
+    let token = req.body.token
+
+    if(!token) return res.status(401).json({msg:'not authorized'})
+
+    jwt.verify(token,process.env.TOKEN_SECRET,(err,user) => {
+        if(err) return res.status(400).json({msg:'token does not match'})
+
+        req.userId = user.id
+        req.userType = user.type
+        next()
+    })
+}
 
 module.exports = Router
--- validation.js
+++ validation.js
@@ -24,13 +24,8 @@ const changePasswordValidation = data => {
 
 const registerValidation = data => {
     const schema = Joi.object({
-        name : Joi.string().min(3).required(),
         email : Joi.string().required().email(),
-        regno : Joi.string().required().min(15).max(15),
-        password : Joi.string().min(8).required(),
-        dept : Joi.string().min(3).required(),
-        year : Joi.required(),
-        domain : Joi.required()
+        password : Joi.string().min(8).required()
     })
 
     return schema.validate(data)
